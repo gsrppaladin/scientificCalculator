b593af600be215ca5aa5e0376c99debe80c9c9b3
Sam Greenhill <samgreenhill@Sams-MBP.home>
2017-03-12 20:25:01 -0400

    setting up all the iboutlets and actions

--- ScientificCalc/ViewController.swift
+++ ScientificCalc/ViewController.swift
@@ -10,16 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    
     @IBOutlet var leftStackViewLeftContraint: NSLayoutConstraint!
-    
-    
     @IBOutlet var rightStackViewLeftConstraint: NSLayoutConstraint!
+    
+    @IBOutlet var resultLbl: UILabel!
+    
+    var op:Int = 0 //track which operator was pressed.
+    var setZero: Bool = true
+    var operand: Float = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setZero = true
+        op = 0
+        resultLbl.text = "0"
     }
+    
+    
+    @IBAction func numericPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func operatorPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func clearPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func equalsPressed(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     override func viewWillLayoutSubviews() {
